cfd7c0e9e9b3ba19d02041229eae29088217a26b
unknown <dongwoo.jeong@outlook.com>
2021-09-27 00:07:28 -0400

    flowcontrol_4_if

---
+++ 4_2_2_if_app.py
@@ -0,0 +1,13 @@
+input_id = input('id : ')
+id = 'egoing'
+if input_id == id:
+    print('Welcome')
+if input_id != id:
+    print('Who?')
+print('---')
+input_id = input('id : ')
+id = 'egoing'
+if input_id == id:
+    print('Welcome')
+else:
+    print('Who?')
